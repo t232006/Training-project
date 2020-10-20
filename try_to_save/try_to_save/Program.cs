553ae0cfd94aae2a89b95a48363fb6b00fb951ce
Tim <t232006@yandex.ru>
2020-10-21 00:31:08 +0500

    не хуйня

--- try_to_save/try_to_save/Program.cs
+++ try_to_save/try_to_save/Program.cs
@@ -8,7 +8,6 @@ namespace try_to_save
         {
             Console.WriteLine("Hello World!");
             Console.ReadKey();
-            lock'ltltye rtkl;hk dfl;gk hdl;fhhdfgh m,dfghdfgh dfh 'ghkdfghl d''
         }
     }
 }
