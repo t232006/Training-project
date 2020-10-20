09a3c751d4d8db398813e487387ca53d5c2c52c9
Tim <t232006@yandex.ru>
2020-10-21 00:18:49 +0500

    first actions

---
+++ try_to_save/try_to_save/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace try_to_save
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
