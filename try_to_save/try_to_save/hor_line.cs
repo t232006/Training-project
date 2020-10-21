d82e653a378c98b152ed214af65dcec1717f99b9
Tim <t232006@yandex.ru>
2020-10-21 20:04:15 +0500

    +hor line

---
+++ try_to_save/try_to_save/hor_line.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace try_to_save
+{
+    class hor_line
+    {
+        List<Point> Cont = new List<Point>();
+        public hor_line(int x1, int x2, int y, char sym)
+        {
+
+            for (int i = x1; i < x2; i++)
+            {
+                Point p = new Point(y, i, sym);
+                Cont.Add(p);
+            }
+        }
+        public void Draw()
+        {
+            foreach (Point i in Cont)
+            {
+                i.Draw();
+            }
+        }
+    }
+}
